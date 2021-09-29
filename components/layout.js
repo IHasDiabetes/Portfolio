21b17dcc1e5e0c9854bea99b23159dc1efa2e283
IHasDiabetes <sadudee.the@student.sk.ac.th>
2021-09-29 15:27:39 +0700

    first deployment commit

---
+++ components/layout.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import styles from '../styles/layout.module.css'
+
+export default function Layout({ children }) {
+    return(
+        <>
+            <div className={styles.container}>
+                <div className={styles.github}>logo</div>
+                <div className={styles.navigator}>
+                        <Link href="/"><a>Home</a></Link>
+                        <Link href="/about"><a>About</a></Link>
+                        <Link href="/skills"><a>Skills</a></Link>
+                        <Link href="/projects"><a>Projects</a></Link>
+                        <Link href="/contacts"><a>Contacts</a></Link>
+                </div>
+                <div className={styles.sidebar}></div>
+            </div>
+            {children}
+        </>
+    )
+}
